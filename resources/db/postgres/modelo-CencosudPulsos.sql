bb1766859c1098060bf9f1e8b8a37e6970574edb
Jorge Guajardo <jguajardo87@gmail.com>
2011-04-18 23:46:12 -0300

    cambios en conf

--- resources/db/postgres/modelo-CencosudPulsos.sql
+++ resources/db/postgres/modelo-CencosudPulsos.sql
@@ -1,12 +1,112 @@
--- Table: "VARIABLES"
+-- Table: bitacoras
 
--- DROP TABLE "VARIABLES";
+-- DROP TABLE bitacoras;
 
-CREATE TABLE "VARIABLES"
+CREATE TABLE bitacoras
 (
-  "ID" integer NOT NULL,
-  "CODIGO_MODEM" integer,
-  "ID_CANAL" integer,
+  id integer NOT NULL DEFAULT nextval('seq_id_bitacora'::regclass),
+  id_remarcador integer,
+  id_usuario integer,
+  fecha_inicial integer,
+  fecha_final integer,
+  CONSTRAINT bitacoras_pkey PRIMARY KEY (id)
+);
+
+-- Table: centros_costos
+
+-- DROP TABLE centros_costos;
+
+CREATE TABLE centros_costos
+(
+  id_centro_costo smallint NOT NULL,
+  nombre_centro_costo character varying(50),
+  CONSTRAINT centros_costos_pkey PRIMARY KEY (id_centro_costo)
+);
+
+-- Table: cuentas
+
+-- DROP TABLE cuentas;
+
+CREATE TABLE cuentas
+(
+  id_cuenta smallint NOT NULL,
+  nombre_cuenta character varying(50),
+  CONSTRAINT cuentas_pkey PRIMARY KEY (id_cuenta)
+);
+
+-- Table: logs_accesos
+
+-- DROP TABLE logs_accesos;
+
+CREATE TABLE logs_accesos
+(
+  id integer NOT NULL DEFAULT nextval('seq_id_log_acceso'::regclass),
+  id_usuario integer,
+  fecha_acceso integer,
+  hora_acceso integer,
+  CONSTRAINT logs_accesos_pkey PRIMARY KEY (id)
+);
+
+-- Table: perfiles
+
+-- DROP TABLE perfiles;
+
+CREATE TABLE perfiles
+(
+  id smallint NOT NULL,
+  nombre character varying(50),
+  descripcion character varying(250),
+  codigo character varying(250),
+  CONSTRAINT perfiles_pkey PRIMARY KEY (id)
+);
+
+-- Table: remarcadores
+
+-- DROP TABLE remarcadores;
+
+CREATE TABLE remarcadores
+(
+  id integer NOT NULL,
+  nombre character varying(50),
+  local_remarcador character varying(50),
+  marca_remarcador character varying(50),
+  modelo_remarcador character varying(50),
+  multiplicador real,
+  tablero character varying(50),
+  numero_medidor character varying(50),
+  nodo character varying(50),
+  observacion character varying(2000),
+  id_centro_costo smallint,
+  id_cuenta smallint,
+  CONSTRAINT remarcadores_pkey PRIMARY KEY (id)
+);
+
+-- Table: usuarios
+
+-- DROP TABLE usuarios;
+
+CREATE TABLE usuarios
+(
+  id smallint NOT NULL,
+  usuario character varying(50),
+  clave character varying(50),
+  nombre character varying(50),
+  apellido_paterno character varying(50),
+  apellido_materno character varying(50),
+  email character varying(50),
+  id_perfil smallint,
+  CONSTRAINT usuarios_pkey PRIMARY KEY (id)
+);
+
+-- Table: variables
+
+-- DROP TABLE variables;
+
+CREATE TABLE variables
+(
+  id integer NOT NULL,
+  codigo_modem integer,
+  id_canal integer,
   basededatos character varying(50),
   usuario_basededatos character varying(50),
   clave_basededatos character varying(50),
@@ -16,79 +116,10 @@ CREATE TABLE "VARIABLES"
   columna_dato character varying(50),
   columna_fecha character varying(50),
   tipo_dato character varying(30),
-  "ID_REMARCADOR" integer,
-  CONSTRAINT variables_pkey PRIMARY KEY ("ID")
-)
+  id_remarcador integer,
+  CONSTRAINT variables_pkey PRIMARY KEY (id)
+);
 
--- Table: "USUARIOS"
 
--- DROP TABLE "USUARIOS";
 
-CREATE TABLE "USUARIOS"
-(
-  "ID" smallint NOT NULL,
-  "USUARIO" character varying(50),
-  "PASSWORD" character varying(50),
-  "NOMBRE" character varying(50),
-  "APELLIDO_PATERNO" character varying(50),
-  "APELLIDO_MATERNO" character varying(50),
-  "EMAIL" character varying(50),
-  "ID_PERFIL" smallint,
-  CONSTRAINT id_usuario PRIMARY KEY ("ID")
-)
 
--- Table: "REMARCADORES"
-
--- DROP TABLE "REMARCADORES";
-
-CREATE TABLE "REMARCADORES"
-(
-  "ID" integer NOT NULL,
-  "NOMBRE" character varying(50),
-  "LOCAL" character varying(50),
-  "MARCA_REMARCADOR" character varying(50),
-  "MODELO_REMARCADOR" character varying(50),
-  "MULTIPLICADOR" real,
-  "TABLERO" character varying(50),
-  "NUMERO_MEDIDOR" character varying(50),
-  "NODO" character varying(50),
-  "OBSERVACION" character varying(2000),
-  "ID_CENTRO_COSTO" smallint,
-  "ID_CUENTA" smallint,
-  CONSTRAINT remarcadores_pkey PRIMARY KEY ("ID")
-)
-
--- Table: "PERFILES"
-
--- DROP TABLE "PERFILES";
-
-CREATE TABLE "PERFILES"
-(
-  "ID" smallint NOT NULL,
-  "NOMBRE" character varying(50),
-  "DESCRIPCION" character varying(250),
-  "CODIGO" character varying(250),
-  CONSTRAINT id_perfil PRIMARY KEY ("ID")
-)
-
--- Table: "CUENTAS"
-
--- DROP TABLE "CUENTAS";
-
-CREATE TABLE "CUENTAS"
-(
-  "ID_CUENTA" smallint NOT NULL,
-  "NOMBRE_CUENTA" character varying(50),
-  CONSTRAINT "CUENTAS_pkey" PRIMARY KEY ("ID_CUENTA")
-)
-
--- Table: "CENTROS_COSTOS"
-
--- DROP TABLE "CENTROS_COSTOS";
-
-CREATE TABLE "CENTROS_COSTOS"
-(
-  "ID_CENTRO_COSTO" smallint NOT NULL,
-  "NOMBRE_CENTRO_COSTO" character varying(50),
-  CONSTRAINT "CENTROS_COSTOS_pkey" PRIMARY KEY ("ID_CENTRO_COSTO")
-)
